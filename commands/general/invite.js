5c21452430d8a1479a6837a40a99df28c239a002
Skybertronic <72767007+Skybertronic@users.noreply.github.com>
2021-04-23 22:07:47 +0200

    Update invite.js
    removed invite

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -3,6 +3,6 @@ module.exports = {
   description: 'Sends an invite link to invite the bot to other servers',
 
   execute(message) {
-    message.reply('https://discord.com/api/oauth2/authorize?client_id=830909126418104382&permissions=268437568&redirect_uri=http%3A%2F%2Flocalhost%3A5000&scope=bot')
+    message.reply("This bot is just a copy")
   }
 }
