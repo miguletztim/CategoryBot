6394a061eeff66fd7183c9ae35fda7e1608b06b7
Skybertronic <72767007+Skybertronic@users.noreply.github.com>
2021-04-29 00:06:44 +0200

    bugfix
    Required to iterate asynchronously

--- functions/colorrole/createColorRole.js
+++ functions/colorrole/createColorRole.js
@@ -4,7 +4,7 @@ module.exports = function(username, position, baseRole) {
   // console.log(`BOT ROLE POSITION:  ${botRole.position}`); // FIXBUG: doesn't change => if more then one role is created, it doesn't get placed at the right position
 
   if(!findColorRole(baseRole)) {
-    baseRole.guild.roles.create({
+    return baseRole.guild.roles.create({
       data: {
         name: baseRole.name + baseRole.hexColor,
         color: baseRole.color,
@@ -15,8 +15,5 @@ module.exports = function(username, position, baseRole) {
     })
     .then(created => console.log(`Created color-role ${created.name}`))
     .catch(console.error);
-    return true;
   }
-  
-  return false;
 }
