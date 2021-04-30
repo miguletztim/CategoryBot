6c3e518460f2de3d6eea7973678aaa58635de063
Skybertronic <72767007+Skybertronic@users.noreply.github.com>
2021-04-30 16:57:28 +0200

    Update updateColorRole.js
    Created the ability to update to color-roles to the standard color

--- functions/colorrole/updateColorRole.js
+++ functions/colorrole/updateColorRole.js
@@ -1,9 +1,13 @@
+const {noColor} = require('../../config.json');
+
 module.exports = function(colorRole, newRole) {
   
   if(colorRole.hexColor !== newRole.hexColor || !colorRole.name.startsWith(newRole.name)) {
+    const hexColor = newRole.hexColor == noColor ? '#99aab5' : newRole.hexColor;
+
     colorRole.edit({ 
-      name: newRole.name + newRole.hexColor,
-      color: newRole.color,
+      name: newRole.name + hexColor,
+      color: hexColor,
       reason: 'base-role was edited',
     })
     .then(updated => console.log(`Updated color-role to ${updated.name}`))
